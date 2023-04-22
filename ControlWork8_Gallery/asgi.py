1e25ae3cca2b87924e6d881cf0dc596978a21f81
Yerzhanov <dearhauer@gmail.com>
2023-04-22 11:37:46 +0600

    Инициализирован проект

---
+++ ControlWork8_Gallery/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ControlWork8_Gallery project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ControlWork8_Gallery.settings')
+
+application = get_asgi_application()
